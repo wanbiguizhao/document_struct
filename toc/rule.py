50343564a929732b77ebe32193ef737fd7b6b923
wanbiguizhao <shiweishiweihubugui@live.cn>
2023-07-23 23:31:15 +0800

    目前通过规则生成目录

--- toc/rule.py
+++ toc/rule.py
@@ -1,42 +1,98 @@
 # 一些正则式，用于区分不同层级的标题
 
-import re 
+import re
+from typing import Any 
+from config import APP_ROOT
+class Rule:
+    def __init__(self,rid,rule_regex) -> None:
+        self.rid=rid
+        self.regex=re.compile(rule_regex)
+    def __call__(self, text) -> Any:
+        match=self.regex.match(text)
+        if match:
+            return True
+        return False
 
-title_rule_list=[
-    re.compile("第[一二三四五六七八九十]节|")
-]
 
-def re_title():
-    re.compile("第[一二三四五六七八九十]节")
-    """
-    第一节 重要提示
-第二节公司基本情况
-第三节重要事项
-    """
-    re.compile("\d\d?.\d[ ]?")
-    """
-3.1 近3年的主要会计数据和财务指标
-3.2 报告期分季度的主要会计数据
-4.1报告期末及年报披露前一个月末的普通股股东总数、表决权恢复的优先股股东总数和持有特别表决权股份的股东总数及前10名股东情
-    """
-    re.compile("^\d+?[^\.]")
-    """
-    1本年度报告摘要来自年度报告全
-4 中兴华会计师事务所（特殊
-5董事会决议通过的本报
-4股东情况
-    """
-    re.compile("^[ ]*?\d+?[^\.]")
-    """
-4股东情况
-1本年度报告摘要来自年度报告全
-4 中兴华会计师事务所（特殊
-5董事会决议通过的本报
-4股东情况
-    1本年度报告摘要来自年度报告全文，为全面了解本公司的经营成果、财务状况及未来发展规划，投资者应当到www.sse.com.cn网站仔细阅读年度报告全文。
-    2本公司董事会、监事会及董事、监事、高级管理人员保证年度报告内容的真实性、准确性完整性，不存在虚假记载、误导性陈述或重大遗漏，并承担个别和连带的法律责任。
-    公司全体董事出席董事会会议。
-    4 中兴华会计师事务所（特殊普通合伙）为本公司出具了标准无保留意见的审计报告。
-    5董事会决议通过的本报告期利润分配预案或公积金转增股本预案
-    """
-    re.compile("^[ ]*?\d+?[^\.]")
+def infer_toc(doc_text_list,doc_name):
+    # 输入是文本，输出是文字大纲级别的预测
+    rule_list:list[Rule]=[
+    Rule(1,"第[一二三四五六七八九十]节"),
+    Rule(11,"^\s*?[一二三四五六七八九十]、"),
+    Rule(2,"^[ ]*?\d+?[^\.、]"),
+    Rule(3,"[ ]*\d+、"),
+    Rule(4,"[ ]*\d+(．|\.)[^\d]"),
+    Rule(5,"\d\d?\.\d[ ]?"),
+    Rule(6,"[(（][一二三四五六七八九十]+[）)]"),
+    Rule(7,"[(（][1234567890]+[）)]"),
+    Rule(8,"\s+[1234567890]+[）)]\s{0,2}"),
+    Rule(9,"^\s*[①②③]+"),
+    Rule(10,"\d+\.\d+\.\d[^\s]"),
+    Rule(11,"^[ ]*\d+\.\d+\.\d\.\d[^\d\.]"),
+    Rule(12,"^[ ]*\d+\.\d+\.\d\.\d\.\d[^\d]"),
+    ]
+    toc_level_mapping={}# rid:level
+    doc_level_mapping={}
+    current_level=-1
+    level_stack=[]
+    for index,text in enumerate(doc_text_list):
+        if "重要提示" in text:
+            doc_level_mapping[index]=1
+            
+
+        text_level=0
+        hit_flag=False
+        for rule in rule_list:
+            #"".lstrip()
+            # if re.match("(\d+年)|(%)",text):
+            #     continue
+            if rule(text.lstrip()):
+                if re.search("^\d\d\d\d年|^\d+%|\d+尺|\d+MWh",text.lstrip()):# "月" in text.lstrip() or "%" in text  :
+                    continue
+                
+                if hit_flag and len(text)<20:
+                    print("ERROR：",text,rule.regex.pattern)
+                hit_flag=True
+                if len(level_stack)==0:
+                    level_stack.append(rule.rid)
+                    toc_level_mapping[rule.rid]=rule
+                    text_level=1
+                else:
+                    if rule.rid in toc_level_mapping:
+                        while rule.rid!=level_stack[-1]:
+                            rid=level_stack.pop(-1)
+                            del toc_level_mapping[rid]
+                            
+                    else:
+                        level_stack.append(rule.rid)
+                        toc_level_mapping[rule.rid]=rule
+                text_level=len(level_stack)
+                # if text_level>0:
+                #     print(text_level,text,toc_level_mapping[level_stack[-1]].regex.pattern)
+        #text_level=len(level_stack)
+        if index not in doc_text_list:
+            doc_level_mapping[index]=text_level
+        #print(text_level,text[:15])
+
+    with open(f"{APP_ROOT}/tmp/{doc_name}.txt","w") as wf:
+        for index,text_level in doc_level_mapping.items():
+            if len(doc_text_list[index].replace("\n","").strip())==0:
+                continue
+            tab=0
+            if text_level==0:
+                tab=5
+            else:
+                tab=min(5,text_level)
+            wf.write(f"{text_level}{'  '*tab}<->\t{doc_text_list[index].strip()}\n")
+            #print(text_level,"<->",doc_text_list[index].lstrip())        
+    doc_text_list
+
+def pipline01():
+    from data.utils import load_doc_text_data
+    from data.config import DOC_ROOT_PATH
+    doc_info=load_doc_text_data(DOC_ROOT_PATH)
+    for doc_name ,doc_text_list in doc_info.items():
+        print("="*10)
+        infer_toc(doc_text_list,doc_name)
+if __name__=="__main__":
+    pipline01()
